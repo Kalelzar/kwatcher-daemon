627444b3ed28405d83ef106c8e8d1cef1b4a9e40
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-03-02 16:47:05 +0200

    feature: Initial server version

--- src/http.zig
+++ src/http.zig
@@ -1,21 +1,54 @@
 const std = @import("std");
 const tk = @import("tokamak");
+const zmpl = @import("zmpl");
 const api = @import("api.zig");
 const model = @import("model.zig");
+const template = @import("template.zig");
+
+fn notFound(data: *zmpl.Data) !template.Template {
+    _ = try data.object();
+    return template.Template.init("not_found");
+}
 
 const App = struct {
     system_service: model.SystemService,
     server: *tk.Server,
     routes: []const tk.Route = &.{
-        tk.logger(.{}, &.{
-            tk.static.dir("public", .{}),
-            .group("/api/system", &.{.router(api.system)}),
-            .get("/openapi.json", tk.swagger.json(.{ .info = .{ .title = "KWatcher Daemon" } })),
-            .get("/swagger-ui", tk.swagger.ui(.{ .url = "openapi.json" })),
-        }),
+        .get("/", tk.static.file("static/index.html")),
+        template.templates(
+            &.{
+                tk.logger(
+                    .{},
+                    &.{
+                        .group("/api/system", &.{.router(api.system)}),
+                        .get("/openapi.json", tk.swagger.json(.{ .info = .{ .title = "KWatcher Daemon" } })),
+                        .get("/swagger-ui", tk.swagger.ui(.{ .url = "openapi.json" })),
+                        .get("/*", notFound),
+                    },
+                ),
+            },
+        ),
     },
 };
 
 pub fn main() !void {
-    try tk.app.run(App);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const root = tk.Injector.init(&.{
+        &gpa.allocator(),
+        &tk.ServerOptions{ .listen = .{
+            .hostname = "0.0.0.0",
+            .port = 8080,
+        } },
+    }, null);
+
+    var app: App = undefined;
+    const injector = try tk.Module(App).init(&app, &root);
+    defer tk.Module(App).deinit(injector);
+
+    if (injector.find(*tk.Server)) |server| {
+        server.injector = injector;
+        try server.start();
+    }
 }
---
+++ src/template.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const zmpl = @import("zmpl");
+const tk = @import("tokamak");
+
+const TemplateData = struct {};
+const templateData = TemplateData{};
+
+pub const Template = struct {
+    template: zmpl.Template,
+    pub fn init(name: []const u8) !Template {
+        const template = zmpl.find(name) orelse {
+            std.log.err("Tried to instantiate a non-exsistant template '{s}'", .{name});
+            return error.TemplateNotFound;
+        };
+        return .{
+            .template = template,
+        };
+    }
+
+    pub fn sendResponse(self: *const Template, context: *tk.Context) !void {
+        const data = try context.injector.get(*zmpl.Data);
+        return self.sendResponseWithData(context, data);
+    }
+
+    pub fn sendResponseWithData(self: *const Template, context: *tk.Context, data: *zmpl.Data) !void {
+        const body = try self.template.render(data, TemplateData, templateData, .{});
+
+        context.res.header("content-type", "text/html");
+        context.res.header("cache-control", "no-cache, no-store, must-revalidate");
+        std.log.info("Sending: {s}", .{body});
+        context.res.body = body;
+        context.responded = true;
+    }
+};
+
+pub fn templates(children: []const tk.Route) tk.Route {
+    const H = struct {
+        fn handleTemplates(context: *tk.Context) anyerror!void {
+            var data = zmpl.Data.init(context.allocator);
+
+            var tdata = .{
+                .data = &data,
+            };
+
+            return context.nextScoped(&tdata);
+        }
+    };
+    return .{ .handler = &H.handleTemplates, .children = children };
+}
