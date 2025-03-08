5d1603a544a435de54edcbbabeca68628ef21f0f
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-03-09 01:37:21 +0200

    feat: Add up metric

--- src/kwatcher/route.zig
+++ src/kwatcher/route.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 const schema = @import("kwatcher").schema;
 
+const metric = @import("../metrics.zig");
+
 pub fn @"consume amq.direct/metrics"(
     metrics: schema.Metrics.V1(),
-) void {
+) !void {
+    try metric.up(.{
+        .job = metrics.client.name,
+    });
     std.log.info("Got metrics from: [{s}]{s}@{s} - {s}:{s}", .{
         metrics.user.id,
         metrics.user.username,
