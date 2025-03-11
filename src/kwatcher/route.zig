5f768f76432be18e1562f6fa67b90b036eb20923
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-03-11 07:36:56 +0200

    feat: Collect metrics from watchers

--- src/kwatcher/route.zig
+++ src/kwatcher/route.zig
@@ -5,15 +5,17 @@ const metric = @import("../metrics.zig");
 
 pub fn @"consume amq.direct/metrics"(
     metrics: schema.Metrics.V1(),
+    persistent: *std.heap.ArenaAllocator,
 ) !void {
-    try metric.up(.{
+    const alloc = persistent.allocator();
+
+    try metric.collected.put(
+        alloc,
+        try alloc.dupe(u8, metrics.client.name),
+        try alloc.dupe(u8, metrics.metrics),
+    );
+
+    try metric.update(.{
         .job = metrics.client.name,
-    });
-    std.log.info("Got metrics from: [{s}]{s}@{s} - {s}:{s}", .{
-        metrics.user.id,
-        metrics.user.username,
-        metrics.user.hostname,
-        metrics.client.name,
-        metrics.client.version,
     });
 }
