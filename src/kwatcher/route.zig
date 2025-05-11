ac981ace0847965e933e049ec6e737a10cbc4e54
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-05-11 16:07:19 +0300

    feat: Run kwatcher in a separate thread instead of polling on demand

--- src/kwatcher/route.zig
+++ src/kwatcher/route.zig
@@ -6,6 +6,8 @@ const metric = @import("../metrics.zig");
 pub fn @"consume amq.direct/metrics"(
     metrics: schema.Metrics.V1(),
 ) !void {
+    metric.collected_mutex.lock();
+    defer metric.collected_mutex.unlock();
     try metric.collected.put(metrics.client.name, metrics.metrics);
 
     try metric.update(.{
